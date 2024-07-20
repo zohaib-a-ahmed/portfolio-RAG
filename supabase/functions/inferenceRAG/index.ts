1592e4f1289e87485b3da6bd43cb9695985bc447
zahmed <zohaib0718@gmail.com>
2024-07-20 15:32:31 -0500

    feat: rating limiting via redis

--- supabase/functions/inferenceRAG/index.ts
+++ supabase/functions/inferenceRAG/index.ts
@@ -1,15 +1,24 @@
+// deno-lint-ignore-file
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.44.4"
 import OpenAI from "https://deno.land/x/openai@v4.52.7/mod.ts";
+import { Ratelimit } from "https://cdn.skypack.dev/@upstash/ratelimit@latest";
+import { Redis } from "https://esm.sh/@upstash/redis";
 
 const openAiKey = Deno.env.get('OPENAI_API_KEY')!
-const supabaseUrl = Deno.env.get('LOCAL_SUPABASE_URL')!
-const supabaseServiceRoleKey = Deno.env.get('LOCAL_SUPABASE_KEY')!
+const supabaseUrl = Deno.env.get('SUPABASE_URL')!
+const supabaseServiceRoleKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!
+const redisUrl = Deno.env.get("UPSTASH_REDIS_REST_URL")!
+const redisToken = Deno.env.get("UPSTASH_REDIS_REST_TOKEN")!
 
 const openai = new OpenAI({
   apiKey: openAiKey
   });
 const supabase = createClient(supabaseUrl, supabaseServiceRoleKey)
+const redis = new Redis({
+  url: redisUrl,
+  token: redisToken,
+});
 
 serve(async (req) => {
   const body = await req.text();
@@ -17,6 +26,19 @@ serve(async (req) => {
 
   if (!query) {
     throw new Error("Query is required");
+  }
+
+  const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(3, "10 s"),
+    analytics: true,
+    prefix: "@upstash/ratelimit",
+  });
+
+  const identifier = "api";
+  const { success } = await ratelimit.limit(identifier);
+  if (!success) {
+    throw new Error("Rate Limit Exception");
   }
   
   try {
@@ -61,11 +83,13 @@ async function retrieveRelevantDocuments(query: string, embedding: number[]): Pr
 async function generateResponse(query: string, relevantDocuments: any[]): Promise<string> {
   const context = relevantDocuments.map(doc => doc.content).join('\n\n')
   const prompt = `Context: ${context}\n\nQuestion: ${query}\n\nAnswer:`
+  const preliminary = 
+  'You are Zohaib Ahmed, a software engineer and computer science student. Use only the provided context of your experience and accomplishments to answer the question. Limit your responses to 2-3 sentences max.'
   
   const completionResponse = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
     messages: [
-      { role: 'system', content: 'You are a software engineer. Use only the provided context of your experience and accomplishments to answer the question.' },
+      { role: 'system', content: preliminary },
       { role: 'user', content: prompt }
     ],
   })
